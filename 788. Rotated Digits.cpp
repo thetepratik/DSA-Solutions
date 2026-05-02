26ff7f989ec10c64c01d137da964bcec42d78fa1
Pratik Thete <pratikthete77@gmail.com>
2026-05-02 11:25:41 +0530

    Add solution for counting good rotated digits
    Implement a solution to count good integers that remain valid after rotating digits by 180 degrees.

---
+++ 788. Rotated Digits.cpp
@@ -0,0 +1,61 @@
+An integer x is a good if after rotating each digit individually by 180 degrees, we get a valid number that is different from x. Each digit must be rotated - we cannot choose to leave it alone.
+
+A number is valid if each digit remains a digit after rotation. For example:
+
+0, 1, and 8 rotate to themselves,
+2 and 5 rotate to each other (in this case they are rotated in a different direction, in other words, 2 or 5 gets mirrored),
+6 and 9 rotate to each other, and
+the rest of the numbers do not rotate to any other number and become invalid.
+Given an integer n, return the number of good integers in the range [1, n].
+
+ 
+
+Example 1:
+
+Input: n = 10
+Output: 4
+Explanation: There are four good numbers in the range [1, 10] : 2, 5, 6, 9.
+Note that 1 and 10 are not good numbers, since they remain unchanged after rotating.
+Example 2:
+
+Input: n = 1
+Output: 0
+Example 3:
+
+Input: n = 2
+Output: 1
+ 
+
+Constraints:
+
+1 <= n <= 104
+
+Code:
+  class Solution {
+public:
+    bool isGood(int n){
+        bool change = false;
+
+        while( n > 0 ){
+            int digit = n % 10;
+
+            if(digit == 3 || digit == 4 || digit == 7){
+                return false;
+            }else if(digit == 2|| digit == 5 || digit == 6 || digit == 9){
+                change = true;
+            }
+            n /= 10;
+        }
+        return change;
+    }
+
+    int rotatedDigits(int n) {
+        int count = 0;
+        for(int i=0;i<=n;i++){
+            if(isGood(i)){
+                count++;
+            }
+        }
+        return count;
+    }
+};
