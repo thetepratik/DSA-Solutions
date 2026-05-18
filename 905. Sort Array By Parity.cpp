a9de2de3b32b92431e7f86654b0dabb27022657c
Pratik Thete <pratikthete77@gmail.com>
2026-05-18 07:58:02 +0530

    Add function to sort array by parity
    Implement sorting of an array by moving even integers to the front.

---
+++ 905. Sort Array By Parity.cpp
@@ -0,0 +1,32 @@
+Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+Return any array that satisfies this condition.
+ 
+Example 1:
+Input: nums = [3,1,2,4]
+Output: [2,4,3,1]
+Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+ 
+Constraints:
+1 <= nums.length <= 5000
+0 <= nums[i] <= 5000
+
+Code:
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(nums[i] % 2 == 0){
+                ans.insert(ans.begin(), nums[i]);
+            }else{
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+    }
+};
